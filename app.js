777a7fcbfa50b4b4ee58925ef2403d5971037e39
Mathieu Molina <molina.mathieu@hotmail.com>
2022-06-16 14:31:53 +0200

    double API cat

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const img = document.getElementById('img');
-
+const img2 = document.getElementById('img2');
 fetch('https://api.thecatapi.com/v1/images/search/')
     .then(res => {
         if(res.ok){
@@ -11,5 +11,20 @@ fetch('https://api.thecatapi.com/v1/images/search/')
             console.log("ERREUR")
             document.getElementById('erreur').innerHTML = "Erreur :("
         }
+    });
+
+
+    fetch('https://api.thecatapi.com/v1/images/search/')
+    .then(res => {
+        if(res.ok){
+            res.json().then(data => {
+                img2.src = data[0].url
+            })
         }
-    )
+        else{
+            console.log("ERREUR")
+            document.getElementById('erreur').innerHTML = "Erreur :("
+        }
+    });
+
+    //! Attention : Il faut obligatoirement utiliser json pour faire fonctionner les API
